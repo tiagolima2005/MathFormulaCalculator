0892e92a6e7ea5334aef0dc7fe9eeac6fc94cca1
tiagolima2005 <tilima.steam@gmail.com>
2024-10-19 20:30:56 +0100

    first look at the circle formulas

--- Main/MathCalculator.C
+++ Main/MathCalculator.C
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
+void circleFormulas();
+
 int main()
 {
     int option;
@@ -11,5 +13,52 @@ int main()
     printf("4 - Rectangle Formulas\n");
     printf("5 - Sphere Formulas\n");
     printf("6 - Cylinder Formulas\n");
-    scanf("%d" , option);
+    scanf("%d" , &option);
+
+    switch (option)
+    {
+    case 1:
+        circleFormulas();
+        break;
+    
+    default:
+        break;
+    }
+
+    return 0;
+}
+
+void circleFormulas()
+{
+    double Radius;
+    int circleAnswer;
+
+    while (1)
+    {
+    printf("Choose one of the following circle formulas:\n");
+    printf("1 - Circumference (C)\n");
+    printf("2 - Area (A)\n");
+    scanf("%d" , &circleAnswer);
+
+    if (circleAnswer == 1)
+    {
+        printf("Insert the Radius of the circle:\n");
+        scanf("%lf" , &Radius);
+        double circumference = (2 * M_PI * Radius);
+        printf("The Circumference of your circle is: %.2lf" , circumference);
+        break;
+    }
+    else if (circleAnswer == 2)
+    {
+        printf("Insert the Radius of the circle:\n");
+        scanf("%lf" , &Radius);
+        double area = (M_PI * (Radius * Radius));
+        printf("The Area of your circle is: %.2lf" , area);
+        break;
+    }
+    else 
+    {
+        printf("Invalid number, please choose between 1 and 2...\n");
+    }
+    }
 }
