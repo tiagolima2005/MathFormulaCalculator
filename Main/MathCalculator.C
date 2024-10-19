d25a1d70708fdc6bd07b13c1e70672067ce9e2e2
tiagolima2005 <tilima.steam@gmail.com>
2024-10-19 20:02:33 +0100

    first look at the menu

---
+++ Main/MathCalculator.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int option;
+    printf("Enter the number of the formula you want:\n");
+    printf("1 - Circle Formulas\n");
+    printf("2 - Triangle Formulas\n");
+    printf("3 - Square Formulas\n");
+    printf("4 - Rectangle Formulas\n");
+    printf("5 - Sphere Formulas\n");
+    printf("6 - Cylinder Formulas\n");
+    scanf("%d" , option);
+}
